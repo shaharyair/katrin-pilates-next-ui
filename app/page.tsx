baaba921ee22e5479a7c44c2edf1ed6de13482c4
Shahar Yair <shaharyair12@gmail.com>
2024-12-20 23:34:20 +0200

    add home page metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import { Metadata } from "next";
+
 import { Hero } from "@/components/hero";
+
+export const metadata: Metadata = {
+  title: "הסטודיו של קאתרין",
+};
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col items-center justify-center">
       <Hero />
     </div>
   );
