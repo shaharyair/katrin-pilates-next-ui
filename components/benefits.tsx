a4074989623e904df53100839edaf98101c051d7
Shahar Yair <shaharyair12@gmail.com>
2024-12-27 23:01:20 +0200

    add benefits component
    - add new icons - (clock, users, calendar, dumbbell)
    - change site config file to tsx - to use components as props

---
+++ components/benefits.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/card";
+
+import { Container } from "./container";
+import { SectionWrapper } from "./sectionWrapper";
+
+import { siteConfig } from "@/config/site";
+
+const sectionWrapperProps = {
+  title: siteConfig.benefits.title,
+  classNames: {
+    childrenWrapper:
+      "w-full grid grid-cols-1 md:grid-cols-4 gap-6 auto-rows-auto",
+  },
+};
+
+export const Benefits = () => {
+  return (
+    <Container>
+      <SectionWrapper {...sectionWrapperProps}>
+        {siteConfig.benefits.items.map((item, i) => (
+          <BenefitsCard key={i} {...item} />
+        ))}
+      </SectionWrapper>
+    </Container>
+  );
+};
+
+const BenefitsCard = (props: (typeof siteConfig.benefits.items)[0]) => {
+  const iconWithProps = React.cloneElement(props.icon, {
+    width: 40,
+    height: 40,
+  });
+
+  return (
+    <Card
+      isHoverable
+      classNames={{
+        base: "p-4",
+        header: "flex justify-center",
+        body: "text-center",
+      }}
+    >
+      <CardHeader>{iconWithProps}</CardHeader>
+      <CardBody>
+        <p>{props.text}</p>
+      </CardBody>
+    </Card>
+  );
+};
--- components/icons.tsx
+++ components/icons.tsx
@@ -213,3 +213,95 @@ export const NextUILogo: React.FC<IconSvgProps> = (props) => {
     </svg>
   );
 };
+
+export const DumbbellIcon: React.FC<IconSvgProps> = (props) => {
+  const { width, height = 24 } = props;
+
+  return (
+    <svg
+      fill="none"
+      height={width}
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      width={height}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M14.4 14.4 9.6 9.6" />
+      <path d="M18.657 21.485a2 2 0 1 1-2.829-2.828l-1.767 1.768a2 2 0 1 1-2.829-2.829l6.364-6.364a2 2 0 1 1 2.829 2.829l-1.768 1.767a2 2 0 1 1 2.828 2.829z" />
+      <path d="m21.5 21.5-1.4-1.4" />
+      <path d="M3.9 3.9 2.5 2.5" />
+      <path d="M6.404 12.768a2 2 0 1 1-2.829-2.829l1.768-1.767a2 2 0 1 1-2.828-2.829l2.828-2.828a2 2 0 1 1 2.829 2.828l1.767-1.768a2 2 0 1 1 2.829 2.829z" />
+    </svg>
+  );
+};
+
+export const UsersIcon: React.FC<IconSvgProps> = (props) => {
+  const { width, height = 24 } = props;
+
+  return (
+    <svg
+      fill="none"
+      height={height}
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      width={width}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+      <circle cx="9" cy="7" r="4" />
+      <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
+      <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+    </svg>
+  );
+};
+
+export const ClockIcon: React.FC<IconSvgProps> = (props) => {
+  const { width, height = 24 } = props;
+
+  return (
+    <svg
+      fill="none"
+      height={height}
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      width={width}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="12" cy="12" r="10" />
+      <polyline points="12 6 12 12 16 14" />
+    </svg>
+  );
+};
+
+export const CalendarIcon: React.FC<IconSvgProps> = (props) => {
+  const { width, height = 24 } = props;
+
+  return (
+    <svg
+      fill="none"
+      height={height}
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      width={width}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M8 2v4" />
+      <path d="M16 2v4" />
+      <rect height="18" rx="2" width="18" x="3" y="4" />
+      <path d="M3 10h18" />
+    </svg>
+  );
+};
