b7ceacad0931fed5f38664c13bbef86ca1242152
Shahar Yair <shaharyair12@gmail.com>
2024-12-28 16:21:07 +0200

    fix: footer spacing
    - reduce social icons size

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,27 +8,29 @@ import { Logo } from "./logo";
 export const Footer = () => {
   return (
     <footer className="w-full">
-      <Container className="flex w-full flex-col gap-2 !pb-6">
-        <Divider className="mb-2" />
+      <Container className="flex w-full flex-col gap-6 !pb-6 !pt-0">
+        <Divider />
         <div className="flex w-full items-center justify-between">
           <SocialLinks />
           <Logo />
         </div>
-        <div className="flex w-full items-start justify-center gap-4">
-          <Link color="foreground" href="/מדיניות-פרטיות">
-            מדיניות פרטיות
-          </Link>
-          <Link color="foreground" href="/תנאי-שימוש">
-            תנאי שימוש
-          </Link>
-          <Link color="foreground" href="/תקנון-אתר">
-            תקנון אתר
-          </Link>
-        </div>
-        <div>
-          <p className="text-center text-sm text-gray-500 dark:text-gray-400">
-            הסטודיו של קאתרין. כל הזכויות שמורות 2024 ©
-          </p>
+        <div className="flex flex-col items-center justify-center gap-2">
+          <div className="flex w-full items-start justify-center gap-4">
+            <Link color="foreground" href="/מדיניות-פרטיות">
+              מדיניות פרטיות
+            </Link>
+            <Link color="foreground" href="/תנאי-שימוש">
+              תנאי שימוש
+            </Link>
+            <Link color="foreground" href="/תקנון-אתר">
+              תקנון אתר
+            </Link>
+          </div>
+          <div>
+            <p className="text-center text-sm text-gray-500 dark:text-gray-400">
+              הסטודיו של קאתרין. כל הזכויות שמורות 2024 ©
+            </p>
+          </div>
         </div>
       </Container>
     </footer>
