3cfe878aa5c2bc59354319a1a1f96c8f78d6d89f
Shahar Yair <shaharyair12@gmail.com>
2024-12-28 16:28:22 +0200

    add not found - 404 error page

---
+++ app/not-found.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Card, CardBody, CardFooter } from "@nextui-org/card";
+import { Link } from "@nextui-org/link";
+import { useRouter } from "next/navigation";
+
+import { Container } from "@/components/container";
+
+export default function NotFound() {
+  const router = useRouter();
+
+  return (
+    <Container className="!py-0">
+      <Card
+        isHoverable
+        isPressable
+        classNames={{
+          base: "w-full lg:w-1/2 m-auto",
+          body: "flex justify-center items-center flex-col gap-4",
+        }}
+        onPress={() => router.replace("/")}
+      >
+        <CardBody>
+          <h1 className="text-[7.5rem] font-semibold leading-[8rem]">404</h1>
+          <p className="text-xl">הדף שחיפשתם לא נמצא</p>
+          <Link className="text-xl" href="/" underline="hover">
+            חזרו לדף הבית
+          </Link>
+        </CardBody>
+        <CardFooter />
+      </Card>
+    </Container>
+  );
+}
