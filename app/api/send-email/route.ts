bc73a97028199d5685fac29f1b388eeede80259c
Shahar Yair <shaharyair12@gmail.com>
2025-01-04 01:11:31 +0200

    add send-email route for contact us form
    - use web3forms api to send contact us emails

---
+++ app/api/send-email/route.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+const WEB3FORMS_API_URL = "https://api.web3forms.com";
+const WEB3FORMS_API_KEY = process.env.WEB3FORMS_API_KEY || "";
+
+const axiosInstance = axios.create({
+  baseURL: WEB3FORMS_API_URL,
+});
+
+const EMAIL_SUBJECT = "New message from Katrin's Studio";
+
+const buildRequestBody = (data: any) => ({
+  ...data,
+  subject: EMAIL_SUBJECT,
+  access_key: WEB3FORMS_API_KEY,
+});
+
+export async function POST(request: NextRequest) {
+  try {
+    const json = await request.json();
+    const res = await axiosInstance.post("/submit", buildRequestBody(json));
+
+    return NextResponse.json(res.data, { status: 200 });
+  } catch (error: any) {
+    return NextResponse.json(
+      { success: false, message: error?.message },
+      { status: 500 },
+    );
+  }
+}
