3b42525e1d1867079f5ccece19dc020921520c8e
Shahar Yair <shaharyair12@gmail.com>
2025-01-25 14:55:53 +0200

    use lightbox component in grid gallery carousel

--- components/gridGalleryCarousel.tsx
+++ components/gridGalleryCarousel.tsx
@@ -1,16 +1,56 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import NextImage from "next/image";
-import { Gallery, GalleryProps, ThumbnailImageProps } from "react-grid-gallery";
+import { useMemo } from "react";
+import {
+  Gallery,
+  GalleryProps,
+  Image,
+  ThumbnailImageProps,
+} from "react-grid-gallery";
 
 import { siteConfig } from "../config/site";
+import { useAppDispatch, useAppSelector } from "../hooks/redux";
+import { setLightboxImage } from "../features/lightbox/lightboxSlice";
 
 import { Container } from "./container";
 import { EmblaCarousel, EmblaCarouselSlide } from "./emblaCarousel";
 
+const Lightbox = dynamic(() => import("../components/lightbox"));
+
 export const GridGalleryCarousel = () => {
+  const lightbox = useAppSelector((state) => state.lightbox.value);
+  const dispatch = useAppDispatch();
+
+  const onLightboxClose = () => {
+    dispatch(setLightboxImage(null));
+  };
+
+  const lightboxSlides = useMemo(() => {
+    const images = siteConfig.galleryGridImages.flatMap((arr) =>
+      arr.map((item) => ({ src: item.src })),
+    );
+
+    return images;
+  }, []);
+
+  const currentLightboxIndex = useMemo(() => {
+    const index = lightboxSlides.findIndex(
+      (slide) => slide.src === lightbox?.src,
+    );
+
+    return index;
+  }, [lightboxSlides, lightbox]);
+
   return (
     <>
+      <Lightbox
+        close={onLightboxClose}
+        index={currentLightboxIndex}
+        open={!!lightbox}
+        slides={lightboxSlides}
+      />
       <Container className="max-w-none px-0 drop-shadow-sm" id="gallery">
         <EmblaCarousel
           options={{ loop: true, slidesToScroll: "auto", displayButtons: true }}
@@ -25,6 +65,12 @@ export const GridGalleryCarousel = () => {
 };
 
 export const Slide = (props: GalleryProps) => {
+  const dispatch = useAppDispatch();
+
+  const handleImageClick = (index: number, image: Image) => {
+    dispatch(setLightboxImage(image));
+  };
+
   return (
     <EmblaCarouselSlide className="basis-full sm:basis-1/2 lg:basis-1/3">
       <Gallery
@@ -33,6 +79,7 @@ export const Slide = (props: GalleryProps) => {
         margin={1}
         maxRows={2}
         thumbnailImageComponent={thumbnailImageComponent}
+        onClick={handleImageClick}
       />
     </EmblaCarouselSlide>
   );
