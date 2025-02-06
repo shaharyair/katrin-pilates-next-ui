2d36a190da33a0810a9aa9146ffbf6f2df62db57
Shahar Yair <shaharyair12@gmail.com>
2025-02-06 23:22:28 +0200

    add legal nav items

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,8 @@
 import { Divider } from "@heroui/divider";
 import { Link } from "@heroui/link";
 
+import { siteConfig } from "../config/site";
+
 import { Container } from "./container";
 import { SocialLinks } from "./social";
 import { Logo } from "./logo";
@@ -16,19 +18,19 @@ export const Footer = () => {
         </div>
         <div className="flex flex-col items-center justify-center gap-2">
           <div className="flex w-full items-start justify-center gap-4">
-            <Link color="foreground" href="/מדיניות-פרטיות">
-              מדיניות פרטיות
-            </Link>
-            <Link color="foreground" href="/תנאי-שימוש">
-              תנאי שימוש
-            </Link>
-            <Link color="foreground" href="/תקנון-אתר">
-              תקנון אתר
-            </Link>
+            {siteConfig.legalNavItems.map((item, index) => (
+              <Link
+                key={`legal_nav_link_${index}`}
+                color="foreground"
+                href={item.href}
+              >
+                {item.label}
+              </Link>
+            ))}
           </div>
           <div>
             <p className="text-center text-sm text-gray-500 dark:text-gray-400">
-              הסטודיו של קאתרין. כל הזכויות שמורות 2024 ©
+              הסטודיו של קאתרין. כל הזכויות שמורות {new Date().getFullYear()} ©
             </p>
           </div>
         </div>
